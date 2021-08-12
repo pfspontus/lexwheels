5c3fcf53d11d750e455b15a3c3a49864201935ce
Pontus <pontus.stromdahl@gmail.com>
2021-08-12 13:52:42 +0200

    merged models_sample with main

---
+++ models/__init__.py
@@ -0,0 +1,67 @@
+"""
+Call create_db to setup database with tables
+
+Example
+    >>> import models
+
+    To create the tables car and owner in the database
+    >>> models.create_db()
+
+    And fill it with mock data
+    >>> models.fill_db_with_mocks()
+
+    Query for a owner filled with owned cars
+    >>> owner_id = 1
+    >>> models.one_owner_with_cars(owner_id)
+    Owner(id=1, name='Gwen Bartlomieczak',
+          cars=[Car(id=84, owner_id=1, make='Dodge', model='Ram 1500', year=2007, owner=None)])
+"""
+import sqlite3
+
+from models import cars
+from models import owners
+
+# conn = sqlite3.connect('db/lexwheels.db')
+
+
+def create_db(conn):
+    """
+    Creates tables car and owner in the database.
+    The database file is located in the db folder
+    """
+    try:
+        cursor = conn.cursor()
+        tables = [cars, owners]
+        for table in tables:
+            cursor.execute(table.create_table_sql())
+    except sqlite3.Error as e:
+        print('Error message:', e.args)
+
+
+def fill_db_with_mocks(conn):
+    """
+    Fills tables car and owner with mock data for testing purposes.
+    """
+    try:
+        cursor = conn.cursor()
+        tables = ['cars.sql', 'owners.sql']
+        for filename in tables:
+            with open(f'mock_data/{filename}', 'rt') as fd:
+                for line in fd.readlines():
+                    cursor.execute(line)
+                conn.commit()
+    except sqlite3.Error as e:
+        print('Error message:', e.args)
+
+
+def one_owner_with_cars(conn, id: int) -> owners.Owner:
+    """
+    Gets owner associated with id. Fills the cars
+    attribute with the cars owned.
+
+    Returns a Owner namedtuple
+    """
+    owner = owners.one(conn, id)
+    owner_cars = cars.with_owner(conn, owner)
+    owner.cars.extend(owner_cars)
+    return owner
---
+++ models/owners.py
@@ -0,0 +1,78 @@
+"""
+Owner data
+
+Example
+    >>> from models import conn
+    >>> from models import owners
+    >>> ids = 1
+    >>> owners.one(conn, id)
+    Owner(id=1, name='Gwen Bartlomieczak', cars=[])
+"""
+from collections import namedtuple
+
+Owner = namedtuple('Owner', [
+    'id',
+    'name',
+    'cars',
+], defaults=([],))
+
+
+def create_table_sql() -> str:
+    """
+    SQL query string to create the owner table.
+    """
+    return 'create table owner (id int, name varchar(100))'
+
+
+def some_sql(ids: tuple) -> str:
+    """
+    SQL query string to select owners associated with given ids.
+    """
+    cols = 'owner.id, owner.name'
+    query = f'select {cols} from owner where id in { ids }'
+    return query
+
+
+def some(conn, ids: tuple, opts=None) -> list:
+    """
+    Returns a list of Owner namedtuples associated with given ids.
+    """
+    return _execute(conn, some_sql(ids))
+
+
+def one_sql(id: int) -> str:
+    """
+    SQL query string to select owner associated with given id.
+    """
+    cols = 'owner.id, owner.name'
+    query = f'select {cols} from owner where owner.id={id}'
+    return query
+
+
+def one(conn, id: int, opts=None) -> Owner:
+    """
+    Returns one Owner namedtuple associated with the given id.
+    """
+    return _execute(conn, one_sql(id))[0]
+
+
+def all_sql() -> str:
+    """
+    SQL query string to select owner associated with given id.
+    """
+    cols = 'owner.id, owner.name'
+    query = f'select {cols} from owner'
+    return query
+
+
+def all(conn, opts=None) -> list:
+    """
+    Returns a list of Owner namedtuples, of all owners.
+    """
+    return _execute(conn, all_sql())
+
+
+def _execute(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return [Owner(*row) for row in cursor.fetchall()]
