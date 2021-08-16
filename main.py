b1a2a41c0af634993afcbb3ce17e93f88f48fb64
Pontus <pontus.stromdahl@gmail.com>
2021-08-16 10:20:01 +0200

    owners and owner details, only show no crud yet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
 
 from models import Models
 from setup_config import setup_config
@@ -6,8 +8,16 @@ from setup_config import setup_config
 app = Flask(__name__)
 setup_config(app)
 models = Models(app)
+Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'ok this app is working now'
+def welcome():
+    owners = models.get_all_owners()
+    return render_template('welcome.html', owners=owners)
+
+
+@app.route('/owner/<int:id>')
+def owner(id):
+    owner = models.get_owner(id)
+    return render_template('owner.html', owner=owner)
